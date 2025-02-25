ff338af010e3331edf89886f2d10976f8798f2ed
DouglasiOSDeveloper <douguerpw2@hotmail.com>
2025-02-25 09:20:39 -0300

    ajuste da navbar direcionável

--- src/app/components/Cta.tsx
+++ src/app/components/Cta.tsx
@@ -1,6 +1,6 @@
 export default function Cta() {
     return (
-      <section className="w-full py-16 bg-blue-600 text-white text-center">
+      <section className="w-full py-16 bg-black-600 text-white text-center">
         <h2 className="text-3xl font-bold">Pronto para impulsionar sua marca?</h2>
         <p className="text-lg mt-2">Entre em contato e dê o próximo passo no digital.</p>
         <button className="mt-6 px-6 py-3 bg-white text-blue-600 font-semibold rounded-xl shadow-lg hover:bg-gray-200 transition">
