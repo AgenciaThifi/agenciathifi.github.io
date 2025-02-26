beb21a6945567cee71512cc161707f73439a7f8f
DouglasiOSDeveloper <douguerpw2@hotmail.com>
2025-02-26 14:00:19 -0300

    a6

--- src/app/components/Destaques.tsx
+++ src/app/components/Destaques.tsx
@@ -44,8 +44,7 @@ export default function Destaques() {
       <motion.div
         className={styles.services}
         drag="x"
-        dragConstraints={{ left: -200, right: 0 }} // Reduzi o limite negativo
-        whileTap={{ cursor: "grabbing" }}
+        dragConstraints={{ left: -50, right: 50 }} // Limita o arraste para evitar espaços em branco
       >
         {servicos.map((servico, index) => (
           <div key={index} className={styles.serviceRow}>
