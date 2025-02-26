5287d0652ba5c127c8dca3fd3fa5b6280f7787a3
DouglasiOSDeveloper <douguerpw2@hotmail.com>
2025-02-26 16:41:12 -0300

    b7

--- src/app/components/Rodape.tsx
+++ src/app/components/Rodape.tsx
@@ -1,8 +1,11 @@
 export default function Rodape() {
-    return (
+  return (
+    <section>
+      {/* Linha separadora entre CTA e Rodapé */}
+      <div className="mt-1 w-full h-0.5 bg-gradient-to-r from-transparent via-white to-transparent opacity-50"></div>
       <footer className="w-full py-6 black text-gray-300 text-center">
         <p>&copy; {new Date().getFullYear()} ThiFi - Agência de Estratégia Digital</p>
       </footer>
-    );
-  }
-  
+    </section>
+  );
+}
