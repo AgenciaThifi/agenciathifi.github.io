a2614ce26626bac00a0678c63890f5f45c946eea
DouglasiOSDeveloper <douguerpw2@hotmail.com>
2025-02-27 12:47:26 -0300

    ajustes finais 1

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,4 +1,9 @@
+import Image from "next/image";
 import styles from "./Hero.module.css"; // Import correto
+import Bdt from "./Imagens/logo_bdt.png";
+import Jardim from "./Imagens/logo_jardimdosidosos.png";
+import Lima from "./Imagens/logo_limaviagens.png";
+import TresP from "./Imagens/logo_trespassos.png";
 
 export default function Hero() {
   return (
@@ -11,7 +16,7 @@ export default function Hero() {
         <p className="text-base md:text-lg font-medium mt-4">
           Estratégia, criatividade e resultados para o seu negócio crescer online.
         </p>
-        
+
         {/* Botão que direciona para o WhatsApp */}
         <a
           href="https://wa.me/61982541672?text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20um%20or%C3%A7amento."
@@ -26,16 +31,18 @@ export default function Hero() {
       {/* Seção Clientes */}
       <div className={styles.clientes}>
         <h2 className="text-3xl md:text-4xl font-bold text-white text-center leading-tight">
-          Junte-se a centenas de <br />
-          negócios que transformaram <br />
-          sua marca com a ThiFi.
+          Junte-se a empresas que <br />
+          impulsionaram suas marcas <br />
+          com a ThiFi e destaque-se <br />
+          no mercado!
         </h2>
 
-        <div className={styles.clientesLogos}>
-          <img src="/logos/cliente1.png" alt="Cliente 1" />
-          <img src="/logos/cliente2.png" alt="Cliente 2" />
-          <img src="/logos/cliente3.png" alt="Cliente 3" />
-          <img src="/logos/cliente4.png" alt="Cliente 4" />
+        {/* Logos dos clientes */}
+        <div className="flex justify-center items-center gap-8 flex-wrap mt-6">
+          <Image src={Bdt} alt="BDT" height={120} />
+          <Image src={Jardim} alt="Jardim dos Idosos" height={200} />
+          <Image src={Lima} alt="Lima Viagens" height={200} />
+          <Image src={TresP} alt="Três Passos" height={200} />
         </div>
       </div>
     </section>
